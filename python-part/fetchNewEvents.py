bdc29bd6c21f04e374eb2ce7df5e1f0745aa97d9
NatsuIce <wangxilin@tongji.edu.cn>
2025-02-19 17:26:48 +0800

    fix: fix logout data type error

--- python-part/fetchNewEvents.py
+++ python-part/fetchNewEvents.py
@@ -380,7 +380,7 @@ def processEvents(session, events):
         "uid": STU_NO,
     }
 
-    logout_data = json.dumps(logout_data, separators=(",", ":"))
+    # logout_data = json.dumps(logout_data, separators=(",", ":"))
 
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
